ec2133a34689a33f209bc3a2dd12b7199f252274
Alfinfdw <113610587+Alfinfdw@users.noreply.github.com>
2022-10-23 06:38:02 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -27,7 +27,7 @@ global.language = indonesia  //change indonesia to english if you don't understa
 // Other
 // Other
 global.botname = "Niluh-Mdོ" //namabot kalian
-global.ownername= "By ᭄ niluhjatim×፝֟͜×" //nama kalian
+global.ownername= "By ᭄ AlxNiluhjatim×፝֟͜×" //nama kalian
 global.myweb ="https://youtube.com/channel/UCOAcuUV1y9wKV8R2xMsOMmg" //bebas asal jan hapus
 global.youtube = "https://youtube.com/channel/UCOAcuUV1y9wKV8R2xMsOMmg" //bebas asal jan hapus
 global.myweb2 = "https://youtube.com/channel/UCOAcuUV1y9wKV8R2xMsOMmg" //bebas
@@ -42,8 +42,8 @@ global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder i
 global.donasi = "./image/donasi.jpg" // foto donasi di folder image
 global.background_welcome="https://telegra.ph/file/90a931648de597820bc08.jpg" // maks size 30kb, agar welcome image nya tdk delay
 global.thumbGif = 'https://telegra.ph/file/e90bef3779d3c5812a5d1.mp4'
-global.packname = '© Niluh-Md-Mdོ' //sticker wm ubah
-global.author = 'alxniluh' //sticker wm ganti nama kalian
+global.packname = '© Niluh-Mdོ' //sticker wm ubah
+global.author = 'Niluh Jatim' //sticker wm ganti nama kalian
 global.sessionName = 'session'
 global.typemenu = 'document'
 global.multiplier = 69
